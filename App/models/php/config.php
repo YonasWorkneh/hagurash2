be9dbdd476dff7a825c6193c2c5cf4424a56b212
YonasWorkneh <yonasdegefu158@gmail.com>
2024-10-17 07:23:51 +0300

    first commit

---
+++ App/models/php/config.php
@@ -0,0 +1,7 @@
+<?php
+$HOST = "127.0.0.1";
+$USER = "root";
+$PASS = "Inc@rrect_1995";
+$DATABASE = "hagurash";
+$hagurash = ["mail" => "yonasdegefu158@gmail.com", "admin" => "Yonas Workneh"];
+$COOKIE_EXPIRATION_DATE = time() + 3600 * 24 * 30;
